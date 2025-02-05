4bddddb734b631221956ab3af60e41fffdf04503
kawgh1 <krseven@gmail.com>
2025-02-05 17:40:12 -0600

    setup dependencies and program.cs

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -1,6 +1,28 @@
+using eCommerce.Core;
+using eCommerce.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Infrastructure services
+builder.Services.AddInfrastructure();
+
+// Add Core services
+builder.Services.AddCore();
+
+// Add controllers to the service collection
+builder.Services.AddControllers();
+
+// Build web app
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Routing
+app.UseRouting();
+
+// Auth
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Controller routes
+app.MapControllers();
 
 app.Run();
---
+++ eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Core;
+
+public static class DependencyInjection {
+
+    /// <summary>
+    /// Extension method to add Core services to the dependency injection container
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        //TODO: Add services to the IoC Container
+        // Core services 
+
+        return services;
+    }
+
+}
